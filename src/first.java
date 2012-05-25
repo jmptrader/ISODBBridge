7fb9613f6cefbd1024dc01013871b425a191e2a0
Saman Kolahdooz <saman.kolahdooz@gmail.com>
2012-05-25 11:18:02 +0430

    the first file that I've pulled into github today

---
+++ src/first.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: saman
+ * Date: 5/25/12
+ * Time: 11:15 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class first {
+    static void main(string arg[]){
+
+    }
+}
